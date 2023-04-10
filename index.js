1fa8fc21a15e44d2de3412e959efa5e867bdbaf5
Krishna Hendra <krisnahendra70@gmail.com>
2023-04-10 14:58:52 +0700

    feat: adjust index project

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.status(200).json({
-        message: "Success",
-        name: "test"
-    })
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success handler GET main",
+  });
+});
+
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Success handler GET testing",
+  });
+});
+
+const port = process.env.port;
+app.listen(port, (req, res) => {
+  console.log(`Server running on port ${port}`);
+});
