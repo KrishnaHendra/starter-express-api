4fffcfd02d2292480b28b23746fb3297682d7ff0
Krishna Hendra <krisnahendra70@gmail.com>
2023-04-11 00:44:10 +0700

    feat: add error handling

--- index.js
+++ index.js
@@ -11,41 +11,49 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/login", async (req, res) => {
-  const {
-    code,
-    shop
-  } = req.query;
-  const apiUrl = process.env.BACKEND_URL;
-  const {
-    data: barcodeData
-  } = await axios.get(
-    `${apiUrl}/bitlogin/api/login/whatsapp/barcode/${shop}?code=${code}`
-  );
+  try {
+    const {
+      code,
+      shop
+    } = req.query;
+    const apiUrl = process.env.BACKEND_URL;
+    const {
+      data: barcodeData
+    } = await axios.get(
+      `${apiUrl}/bitlogin/api/login/whatsapp/barcode/${shop}?code=${code}`
+    );
 
-  res.render("whatsappLogin", {
-    code,
-    barcodeData,
-    apiUrl
-  });
+    res.render("whatsappLogin", {
+      code,
+      barcodeData,
+      apiUrl
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 app.get("/success", async (req, res) => {
-  const {
-    code,
-    type
-  } = req.query;
-  const apiUrl = process.env.BACKEND_URL;
-  const {
-    data
-  } = await axios.get(
-    `${apiUrl}/bitlogin/api/login/whatsapp/status?code=${code}`
-  );
-  const whatsappNumber = process.env.WHATSAPP_NUMBER;
-  res.render("successPage", {
-    data,
-    type,
-    whatsappNumber
-  });
+  try {
+    const {
+      code,
+      type
+    } = req.query;
+    const apiUrl = process.env.BACKEND_URL;
+    const {
+      data
+    } = await axios.get(
+      `${apiUrl}/bitlogin/api/login/whatsapp/status?code=${code}`
+    );
+    const whatsappNumber = process.env.WHATSAPP_NUMBER;
+    res.render("successPage", {
+      data,
+      type,
+      whatsappNumber
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 app.get('/view', (req, res) => {
