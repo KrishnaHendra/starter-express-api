33c02ef922e759fa8786f1c7fcb02150f15da23c
Krishna Hendra <krisnahendra70@gmail.com>
2023-04-12 20:26:58 +0700

    feat: update shop data

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const {
-  default: axios
-} = require("axios");
+const { default: axios } = require("axios");
 const express = require("express");
 const dotenv = require("dotenv");
 
@@ -12,56 +10,49 @@ app.set("view engine", "ejs");
 
 app.get("/login", async (req, res) => {
   try {
-    const {
-      code,
-      shop
-    } = req.query;
+    const { code, shop } = req.query;
     const apiUrl = process.env.BACKEND_URL;
-    const {
-      data: barcodeData
-    } = await axios.get(
+    const { data: barcodeData } = await axios.get(
       `${apiUrl}/bitlogin/api/login/whatsapp/barcode/${shop}?code=${code}`
     );
 
     res.render("whatsappLogin", {
       code,
+      shop,
       barcodeData,
-      apiUrl
+      apiUrl,
     });
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 app.get("/success", async (req, res) => {
   try {
-    const {
-      code,
-      type
-    } = req.query;
+    const { code, type } = req.query;
     const apiUrl = process.env.BACKEND_URL;
-    const {
-      data
-    } = await axios.get(
+    const { data } = await axios.get(
       `${apiUrl}/bitlogin/api/login/whatsapp/status?code=${code}`
     );
     const whatsappNumber = process.env.WHATSAPP_NUMBER;
     res.render("successPage", {
       data,
       type,
-      whatsappNumber
+      whatsappNumber,
     });
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-})
+});
 
-app.get('/view', async (req, res) => {
-  const data = await axios.get("https://api.bitbybit.studio/bitlogin/api/integrations");
+app.get("/view", async (req, res) => {
+  const data = await axios.get(
+    "https://api.bitbybit.studio/bitlogin/api/integrations"
+  );
   res.render("testing", {
-    data
+    data,
   });
-})
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
